d62459f98bcf6e2c52c58061e348c16e39f2bc4e
黄剑飞 <165045765@qq.com>
2020-06-14 17:37:50 +0800

    1

---
+++ src/main/java/demos/formatsql/Formatsql.java
@@ -0,0 +1,33 @@
+package demos.formatsql; 
+/*
+ * Date: 2010-11-9
+ * Time: 9:38:43
+ */
+
+
+import gudusoft.gsqlparser.EDbVendor;
+import gudusoft.gsqlparser.TGSqlParser;
+import gudusoft.gsqlparser.pp.para.GFmtOptFactory;
+import gudusoft.gsqlparser.pp.para.GFmtOpt;
+import gudusoft.gsqlparser.pp.stmtformatter.FormatterFactory;
+
+
+
+public class Formatsql {
+
+    public static void main(String args[])
+     {
+        TGSqlParser sqlparser = new TGSqlParser(EDbVendor.dbvmysql);
+        sqlparser.sqlfilename = "C://a.sql";
+        int ret = sqlparser.parse();
+        if (ret == 0){
+            GFmtOpt option = GFmtOptFactory.newInstance();
+            option.createtableListitemInNewLine = true;
+            String result = FormatterFactory.pp(sqlparser, option);
+            System.out.println(result);
+        }else{
+            System.out.println(sqlparser.getErrormessage());
+        }
+     }
+
+}
