43c299e37441d2feff32ecef6e2087313d0e03be
黄剑飞 <165045765@qq.com>
2020-06-14 19:06:31 +0800

    1

--- src/main/java/demos/formatsql/Formatsql.java
+++ src/main/java/demos/formatsql/Formatsql.java
@@ -7,23 +7,29 @@ package demos.formatsql;
 
 import gudusoft.gsqlparser.EDbVendor;
 import gudusoft.gsqlparser.TGSqlParser;
+
 import gudusoft.gsqlparser.pp.para.GFmtOptFactory;
 import gudusoft.gsqlparser.pp.para.GFmtOpt;
 import gudusoft.gsqlparser.pp.stmtformatter.FormatterFactory;
 
+import java.io.*;
 
 
 public class Formatsql {
 
-    public static void main(String args[])
-     {
+    public static void main(String args[]) throws IOException {
         TGSqlParser sqlparser = new TGSqlParser(EDbVendor.dbvmysql);
-        sqlparser.sqlfilename = "C://a.sql";
+        sqlparser.sqlfilename = new File("").getAbsolutePath()+"/test/a.sql";
         int ret = sqlparser.parse();
         if (ret == 0){
             GFmtOpt option = GFmtOptFactory.newInstance();
-            option.createtableListitemInNewLine = true;
+            option.createtableListitemInNewLine = false;
             String result = FormatterFactory.pp(sqlparser, option);
+            FileOutputStream o = new FileOutputStream(new File(new File("").getAbsolutePath()+"/test/run-result.txt"));
+            OutputStreamWriter out = new OutputStreamWriter(o);
+            out.write(result);
+            out.flush();
+            out.close();
             System.out.println(result);
         }else{
             System.out.println(sqlparser.getErrormessage());
